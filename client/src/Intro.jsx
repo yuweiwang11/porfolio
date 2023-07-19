95e3990b7ba3e057c612ed39cdd796434dbbb535
Yuwei Wang <w67238@outlook.com>
2023-07-19 20:59:23 +1200

    add Intro

--- client/src/Intro.jsx
+++ client/src/Intro.jsx
@@ -1,5 +1,12 @@
 import './Intro.css'
 
 export default function Intro() {
-  return <></>
+  return (
+    <>
+      <div className="hello-and-logo">
+        <div className="logo">👋 </div>
+        <div className="hello"> HELLO, MY NAME IS YUWEI</div>
+      </div>
+    </>
+  )
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import './HomePage.css'
 import Nav from '../Nav'
-
+import Intro from '../Intro'
 export default function HomePage() {
   return (
     <>
       <div className="body">
+        <Nav />
         <div className="margin-body">
-          <Nav />
+          <Intro />
         </div>
       </div>
     </>
